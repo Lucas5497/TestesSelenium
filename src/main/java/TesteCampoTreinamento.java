fe7c44904e3e028ab1285908e49488c78b45f712
Lucas5497 <lucasanselmoluizlopes@gmail.com>
2023-04-15 17:04:58 -0300

    nova classe de testes

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -16,7 +16,7 @@ public class TesteCampoTreinamento {
 		
 		//driver.wait(1000);
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("teste de escrita");
-		
+		//verifica se o que foi escrito está correto
 		Assert.assertEquals("teste de escrita",driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		driver.quit();
 	}
@@ -29,10 +29,9 @@ public class TesteCampoTreinamento {
 		ChromeDriver driver = new ChromeDriver(co);
 		driver.get("https://wcaquino.me/selenium/componentes.html");
 		
-		
 		//procura o elemento através do id, class, name xpath etc. Escreve um texto no elemento
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("testes\n\nselenium\n\nultimalinha\n\n\nops tem mais uma rsrs");
-		
+		//fecha o driver
 		driver.quit();	
 	}
 	
@@ -68,7 +67,21 @@ public class TesteCampoTreinamento {
 		//verificando se o botão está selecionado verifica se o resultado é true
 		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
 		driver.quit();
-	
 	}
 	
+	@Test
+	public void interagirComLista() {
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		System.setProperty("webdriver.chrome.driver", "/Users/lucas/Downloads/chromedriver_win321/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver(co);
+		driver.get("https://wcaquino.me/selenium/componentes.html");
+		
+		//interagindo com o checkBox
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		
+		//verificando se o botão está selecionado verifica se o resultado é true
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+		driver.quit();
+	}
 }
