ee1c0fa784644efbe863e88b30f481e2bb6d17d1
Lucas5497 <lucasanselmoluizlopes@gmail.com>
2023-05-28 21:34:24 -0300

    Automação campo de treinamento | Cadastro

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,87 +1,194 @@
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 
-import junit.framework.Assert;
+
+
 
 public class TesteCampoTreinamento {
-	@Test
-	public void testeTextField() throws InterruptedException{
+	
+	private ChromeDriver driver;
+
+	@Before
+	public void inicializa() {
 		ChromeOptions co = new ChromeOptions();
 		co.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driver", "/Users/lucas/Downloads/chromedriver_win321/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(co);
+		System.setProperty("webdriver.chrome.driverwhitelistedIps", "C://Users//lucas\\OneDrive//Área de Trabalho//chromedriver.exe");
+		driver = new ChromeDriver(co);
 		driver.get("https://wcaquino.me/selenium/componentes.html");
-		
+	}
+
+	@After
+	public void finaliza() {
+		driver.quit();
+	}
+	
+	@Test
+	public void testeTextField() throws InterruptedException{
 		//driver.wait(1000);
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("teste de escrita");
 		//verifica se o que foi escrito está correto
 		Assert.assertEquals("teste de escrita",driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-		driver.quit();
 	}
-	
+
 	@Test
 	public void interagirComTextArea() {
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driver", "/Users/lucas/Downloads/chromedriver_win321/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(co);
-		driver.get("https://wcaquino.me/selenium/componentes.html");
-		
 		//procura o elemento através do id, class, name xpath etc. Escreve um texto no elemento
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("testes\n\nselenium\n\nultimalinha\n\n\nops tem mais uma rsrs");
 		//fecha o driver
-		driver.quit();	
 	}
 	
 	@Test
 	public void interagirComRadioButton() {
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driver", "/Users/lucas/Downloads/chromedriver_win321/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(co);
-		driver.get("https://wcaquino.me/selenium/componentes.html");
-		
-		
 		//interagindo com o radio button
 		driver.findElement(By.id("elementosForm:sexo:0")).click();
-		
 		//verificando se o botão está selecionado verifica se o resultado é true
-		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
-		
-		driver.quit();
+		Assert.assertTrue(!driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
 	}
 	
 	@Test
 	public void interagirComCheckBox() {
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driver", "/Users/lucas/Downloads/chromedriver_win321/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(co);
-		driver.get("https://wcaquino.me/selenium/componentes.html");
-		
 		//interagindo com o checkBox
 		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
-		
 		//verificando se o botão está selecionado verifica se o resultado é true
 		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
-		driver.quit();
 	}
 	
 	@Test
-	public void interagirComLista() {
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driver", "/Users/lucas/Downloads/chromedriver_win321/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(co);
-		driver.get("https://wcaquino.me/selenium/componentes.html");
-		
-		//interagindo com o checkBox
-		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
-		
-		//verificando se o botão está selecionado verifica se o resultado é true
-		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
-		driver.quit();
+	public void interagirComCombo() {
+		//interagindo com a lista | Inicializando a instãncia do webelement 
+		WebElement elemento = driver.findElement(By.id("elementosForm:escolaridade"));
+		//interagindo com o combo
+		Select combo = new Select(elemento);
+		//selecionar através do indice 
+		combo.selectByIndex(3);
+		//selecionar através do value=""
+		combo.selectByValue("superior");
+		//selecionar através do valor apresentado na tela
+		combo.selectByVisibleText("Mestrado");
+		Assert.assertEquals("Mestrado", combo.getFirstSelectedOption().getText());
 	}
-}
+	
+	@Test
+	public void verificarValoresDoCombo() {
+		//interagindo com a lista | Inicializando a instãncia do webelement 
+		WebElement elemento = driver.findElement(By.id("elementosForm:escolaridade"));
+		//interagindo com o combo
+		Select combo = new Select(elemento);
+		List<WebElement> options = combo.getOptions();
+		Assert.assertEquals(8, options.size());	
+		boolean encontrou = false;
+		for(WebElement option: options){
+			if(option.getText().equals("Doutorado")){
+				encontrou = true;
+				break;
+			}
+		}
+		Assert.assertTrue(encontrou);
+	}
+	
+	@Test
+	public void verificarValoresDoComboMultiplo() {
+		//interagindo com a lista | Inicializando a instãncia do webelement 
+		WebElement elemento = driver.findElement(By.id("elementosForm:esportes"));
+		//interagindo com o combo
+		Select combo = new Select(elemento);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Futebol");
+		combo.selectByVisibleText("Corrida");
+	
+		List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectedOptions.size());	
+		
+		combo.deselectByVisibleText("Corrida");
+		
+		
+		List<WebElement> allSelectedOptions2 = combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectedOptions2.size());
+	}
+	
+	@Test 
+	public void interagirComBotao() {
+		
+		driver.findElement(By.id("buttonSimple")).click();
+	}
+	
+	@Test 
+	
+	public void interagirComLink() {	
+		driver.findElement(By.linkText("Voltar")).click();
+		Assert.assertEquals("Voltou!", driver.findElement(By.id("resultado")).getText());
+	}
+	
+	@Test 
+	public void buscarTextosNaPagina() {
+		System.out.print(driver.findElement(By.tagName("Body")).getText());
+		Assert.assertEquals("Campo de Treinamento",
+				driver.findElement(By.tagName("h3")).getText());
+		Assert.assertEquals("Cuidado onde clica, muitas armadilhas...", 
+				driver.findElement(By.className("facilAchar")).getText());
+	}
+	
+	@Test
+	public void interagirComAlert() {	
+		driver.findElement(By.id("Alert")).click();
+		Alert alert = driver.switchTo().alert();
+		
+		String texto = alert.getText();
+		Assert.assertEquals("Alert Simples",texto);
+		alert.accept();
+	}
+	
+	@Test
+	public void interagirComAlertSimples() {
+		driver.findElement(By.id("alert")).click();
+		Alert alert = driver.switchTo().alert();
+		
+		String texto = alert.getText();
+		Assert.assertEquals("Alert Simples",texto);
+		alert.accept();
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+	}
+	
+	@Test
+	public void interagirAlertConfirm() {
+		driver.findElement(By.id("confirm")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.accept();
+		
+		Assert.assertEquals("Confirmado", alerta.getText()); 
+		alerta.accept();
+	}
+
+	public void interagirAlertPrompt() {
+		
+		driver.findElement(By.id("prompt")).click();
+		
+		Alert alertaPrompt = driver.switchTo().alert();
+		
+		Assert.assertEquals("Digite um numero", alertaPrompt.getText());
+		
+		alertaPrompt.sendKeys("12");
+		
+		alertaPrompt.accept();
+		
+		Assert.assertEquals("Era 10? ", alertaPrompt.getText());
+		
+		alertaPrompt.accept();
+		
+		Assert.assertEquals(":D", alertaPrompt.getText());
+		
+		alertaPrompt.accept();
+	}
+}	
--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,4 +1,4 @@
-import org.junit.Assert;
+/*import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -41,4 +41,182 @@ public class TesteGoogle {
 
 
 	}
+}*/
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class TesteGoogle {
+	
+    @Test
+    public void testAllCliAttributes() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("C:\\Users\\lucas\\eclipse-workspace\\lerXmlCadpos\\lerXmlCadpos\\src\\main\\example.xml"));
+
+        NodeList nodeList = document.getElementsByTagName("Cli");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Element cliElement = (Element) nodeList.item(i);
+            
+            //System.out.println("CLI [PASS]");
+            String tipCli = cliElement.getAttribute("TipCli");
+            
+            Assert.assertTrue(tipCli.equals("1") || tipCli.equals("2"));
+            Assert.assertNotNull(tipCli); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", tipCli); // Verifica se o atributo não é vazio
+            System.out.printf("tipCli:"+tipCli+ " [OK] ");
+
+            
+            String idfcCli = cliElement.getAttribute("IdfcCli");
+            //Assert.assertEquals("1", idfcCli);
+            Assert.assertNotNull(idfcCli); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", idfcCli); // Verifica se o atributo não é vazio
+            
+            
+            String nmCli = cliElement.getAttribute("NmCli");
+            //Assert.assertEquals("100000XXXXXXXXXXXXXXXXXXXXXXXXXXXX", nmCli);
+            Assert.assertNotNull(nmCli); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", nmCli); // Verifica se o atributo não é vazio
+            
+            String ntzRlc = cliElement.getAttribute("NtzRlc");
+            //Assert.assertEquals("1", ntzRlc);
+            Assert.assertNotNull(ntzRlc); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", ntzRlc); // Verifica se o atributo não é vazio
+
+            String hstCmt = cliElement.getAttribute("HstCmt");
+            //Assert.assertEquals("1", hstCmt);
+            Assert.assertNotNull(hstCmt); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", hstCmt); // Verifica se o atributo não é vazio
+            
+            String cdOcr = cliElement.getAttribute("CdOcr");
+            //Assert.assertEquals("1", cdOcr);
+            Assert.assertNotNull(cdOcr); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", cdOcr); // Verifica se o atributo não é vazio
+            
+            
+            
+            
+            
+            System.out.println("CLI "+i+" [PASS] ");
+         
+        }
+        
+    }
+
+    @Ignore
+    public void aplicarFor() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("C:\\\\Users\\\\lucas\\\\eclipse-workspace\\\\lerXmlCadpos\\\\lerXmlCadpos\\\\src\\\\main\\\\example.xml"));
+        Element cliElement = (Element) document.getElementsByTagName("Cli").item(0);
+
+        String tipCli = cliElement.getAttribute("TipCli");
+        Assert.assertEquals("1", tipCli);
+
+        String idfcCli = cliElement.getAttribute("IdfcCli");
+        Assert.assertEquals("1", idfcCli);
+
+        String nmCli = cliElement.getAttribute("NmCli");
+        Assert.assertEquals("John Smith", nmCli);
+
+        String ntzRlc = cliElement.getAttribute("NtzRlc");
+        Assert.assertEquals("2", ntzRlc);
+
+        String hstCmt = cliElement.getAttribute("HstCmt");
+        Assert.assertEquals("false", hstCmt);
+
+        String cdOcr = cliElement.getAttribute("CdOcr");
+        Assert.assertEquals("12345", cdOcr);
+    }
+
+    @Test
+    public void testOprAttributes() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("C:\\\\Users\\\\lucas\\\\eclipse-workspace\\\\lerXmlCadpos\\\\lerXmlCadpos\\\\src\\\\main\\\\example.xml"));
+        
+        NodeList nodeList = document.getElementsByTagName("Opr");
+        for(int countOpr = 0;countOpr < nodeList.getLength(); countOpr++) {
+        	
+        	Element oprElement = (Element) document.getElementsByTagName("Opr").item(countOpr);
+		
+		    String nrUnco = oprElement.getAttribute("NrUnco");
+		    //Assert.assertEquals("01", nrUnco);
+		    Assert.assertNotNull(nrUnco);
+		    String prfAg = oprElement.getAttribute("PrfAg");
+		    //Assert.assertEquals("01", prfAg);
+		    Assert.assertNotNull(prfAg);
+		    String nrCtr = oprElement.getAttribute("NrCtr");
+		    //Assert.assertEquals("01", nrCtr);
+		    Assert.assertNotNull(nrCtr);
+		    /*String dtCtrc = oprElement.getAttribute("DtCtrc");
+		    Assert.assertEquals("2022-01-01", dtCtrc);
+		
+		    String cdMdld = oprElement.getAttribute("CdMdld");
+		    Assert.assertEquals("001", cdMdld);
+		
+		    String dtAprc = oprElement.getAttribute("DtAprc");
+		    Assert.assertEquals("2022-01-01", dtAprc);
+		
+		    String cnpjCtrc = oprElement.getAttribute("CnpjCtrc");
+		    Assert.assertEquals("12345678901234", cnpjCtrc);*/
+		    System.out.println("Opr "+countOpr+" [PASS]");
+        }
+        
+    }
+    
+    
+    
+    @Test
+    public void arrumar() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("C:\\Users\\lucas\\eclipse-workspace\\lerXmlCadpos\\lerXmlCadpos\\src\\main\\example.xml"));
+
+        NodeList nodeList = document.getElementsByTagName("Cli");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Element cliElement = (Element) nodeList.item(i);
+            
+            System.out.println("CLI [PASS]");
+            
+            String tipCli = cliElement.getAttribute("TipCli");
+            
+            Assert.assertTrue(tipCli.equals("1") || tipCli.equals("2"));
+            Assert.assertNotNull(tipCli); // Verifica se o atributo não é nulo
+            Assert.assertNotEquals("", tipCli); // Verifica se o atributo não é vazio
+
+
+            
+            /*String idfcCli = cliElement.getAttribute("IdfcCli");
+            Assert.assertEquals("1", idfcCli);
+
+            String nmCli = cliElement.getAttribute("NmCli");
+            Assert.assertEquals("100000XXXXXXXXXXXXXXXXXXXXXXXXXXXX", nmCli);
+
+            String ntzRlc = cliElement.getAttribute("NtzRlc");
+            Assert.assertEquals("1", ntzRlc);
+
+            String hstCmt = cliElement.getAttribute("HstCmt");
+            Assert.assertEquals("1", hstCmt);
+
+            String cdOcr = cliElement.getAttribute("CdOcr");
+            Assert.assertEquals("1", cdOcr);*/
+            System.out.println("CLI "+i+" [PASS]");
+        }
+        
+    }
+
+
 }
