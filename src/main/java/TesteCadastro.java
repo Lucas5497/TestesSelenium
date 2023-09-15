f41e6e8169629eebaf7bb16f057a7efd75fa54e6
Lucas5497 <lucasanselmoluizlopes@gmail.com>
2023-09-15 12:06:47 -0300

    Refatoração dos testes de cadastro usando a DSL e POM

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -2,23 +2,33 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 
 public class TesteCadastro {
 	
-	private ChromeDriver driver;
+	private EdgeDriver driver;
 	private DSL dsl;
+	private CampoTreinamentoPage page;
+	
 	
 	@Before
 	public void inicializar() {
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driverwhitelistedIps", "C://Users//lucas\\OneDrive//Área de Trabalho//chromedriver.exe");
-		driver = new ChromeDriver(co);
+		//ChromeOptions co = new ChromeOptions();
+		//co.addArguments("--remote-allow-origins=*");
+		//System.setProperty("webdriver.chrome.driverwhitelistedIps", "C://Users//lucas\\OneDrive//Área de Trabalho//chromedriver.exe");
+		//driver = new ChromeDriver(co);
+		
+		EdgeOptions options = new EdgeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		System.setProperty("webdriver.edge.driverwhitelistedIps", "C:\\Users\\lucas\\OneDrive\\Área de Trabalho\\msedgedriver.exe");
+		driver = new EdgeDriver(options);
+		
 		driver.get("https://wcaquino.me/selenium/componentes.html");
+		
 		dsl = new DSL(driver);
+		page = new CampoTreinamentoPage(driver);
 	}
 	
 	@After
@@ -27,24 +37,75 @@ public class TesteCadastro {
 	}
 	
 	@Test
-	public void cadastro() {
+	public void deveValidarCadastroComSucesso() throws InterruptedException {
 		
-		dsl.escrever("elementosForm:nome", "Lucas");
-		dsl.escrever("elementosForm:sobrenome", "Lopes");
-		dsl.clicarRadio("elementosForm:sexo:0");
+		page.setNome("Lucas");
+		page.setSobrenome("Lopes");
+		page.setSexoMasculino();
+		page.setComidaVegetariano();
+		page.setEscolaridade("Superior");
+		page.setEsporte("Futebol");
+		page.setSugestoes("Aprenda java e selenium");
+		Thread.sleep(1000);
+		page.cadastrar();
+		Thread.sleep(10000);
+		
+		Assert.assertTrue(page.obterResultadoCadastro().startsWith("Cadastrado!"));
+		Assert.assertTrue(page.obterNomeCadastrado().endsWith("Lucas"));
+		Assert.assertTrue(page.obterSobrenomeCadastrado().endsWith("Lopes"));
 		Assert.assertTrue(dsl.verRadioMarcado("elementosForm:sexo:0"));
-		dsl.clicarCheckBox("elementosForm:comidaFavorita:0");
-		dsl.clicarCheckBox("elementosForm:comidaFavorita:1");
-		Assert.assertTrue(dsl.verCheckBoxMarcado("elementosForm:comidaFavorita:0"));
-		Assert.assertTrue(dsl.verCheckBoxMarcado("elementosForm:comidaFavorita:1"));
-		dsl.selecionarCombo("elementosForm:escolaridade","Superior");
-		Assert.assertEquals("Superior", dsl.pegarValorCombo("elementosForm:escolaridade"));
-		dsl.escrever("elementosForm:sugestoes", "Aprendam Java e Selenium");
-		dsl.clicarBotao("elementosForm:cadastrar");
+		Assert.assertTrue(dsl.verCheckBoxMarcado("elementosForm:comidaFavorita:3"));
 		
-		//System.out.print(dsl.pegarValorDoCampo("resultado"));
-		//Assert.assertTrue("Cadastrado!", driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
-		//Assert.assertTrue("Lucas", driver.findElement(By.id("descNome")).getText().endsWith("Lucas"));
+		Assert.assertEquals("Sexo: Masculino", page.obterSexoCadastrado());
+		Assert.assertEquals("Comida: Vegetariano", page.obterComidaCadastrado());
+		Assert.assertEquals("Esportes: Futebol", page.obterEsporteCadastrado());
+		Assert.assertEquals("Sugestoes: Aprenda java e selenium", page.obterSugestoesCadastradas());
+	
 	}
-
+	
+	@Test
+	public void deveValidarNomeObrigatorio() {
+		page.cadastrar();
+		Assert.assertEquals("Nome eh obrigatorio", dsl.alertaObterTextoAceitar());
+	}
+	
+	@Test
+	public void deveValidarSobrenomeObrigatorio() {
+		page.setNome("Lucas");
+		page.cadastrar();
+		Assert.assertEquals("Sobrenome eh obrigatorio", dsl.alertaObterTextoAceitar());
+	}
+	
+	@Test
+	public void deveValidarComidaVegetariana() {
+		
+		page.setNome("Lucas");
+		page.setSobrenome("Lopes");
+		page.setSexoMasculino();
+		page.setComidaCarne();
+		page.setComidaVegetariano();
+		page.setEscolaridade("Superior");
+		page.setEsporte("Futebol");
+		page.setSugestoes("Aprenda java e selenium");
+		page.cadastrar();
+		
+		Assert.assertEquals("Tem certeza que voce eh vegetariano?", dsl.alertaObterTextoAceitar());
+	
+	}
+	
+	@Test
+	public void deveValidarEsporteIndeciso() {
+		
+		page.setNome("Lucas");
+		page.setSobrenome("Lopes");
+		page.setSexoMasculino();
+		page.setComidaCarne();
+		page.setEscolaridade("Superior");
+		page.setEsporte("Futebol","O que eh esporte?");
+		page.setSugestoes("Aprenda java e selenium");
+		page.cadastrar();
+		
+		Assert.assertEquals("Voce faz esporte ou nao?", dsl.alertaObterTextoAceitar());
+	}
+	
 }
