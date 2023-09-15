a8ac5d777ec35a9d9af58d631afde640b7df471e
Lucas5497 <lucasanselmoluizlopes@gmail.com>
2023-09-15 12:03:43 -0300

    Usando POM na pagina de cadastro

---
+++ src/main/java/CampoTreinamentoPage.java
@@ -0,0 +1,94 @@
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class CampoTreinamentoPage {
+
+	private DSL dsl;
+			
+			
+	public CampoTreinamentoPage(EdgeDriver driver) {
+		dsl = new DSL(driver);
+		
+	}
+	
+	public void setNome(String nome) {
+		dsl.escrever("elementosForm:nome", nome);
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		dsl.escrever("elementosForm:sobrenome", sobrenome);
+	}
+	
+	public void setSexoMasculino() {
+		dsl.clicarRadio("elementosForm:sexo:0");
+	}
+	
+	public void setSexoFeminino() {
+		dsl.clicarRadio("elementosForm:sexo:1");
+	}
+	
+	public void setComidaCarne() {
+		dsl.clicarCheckBox("elementosForm:comidaFavorita:0");
+	}
+	
+	public void setComidaFrango() {
+		dsl.clicarCheckBox("elementosForm:comidaFavorita:1");
+	}
+	
+	public void setComidaPizza() {
+		dsl.clicarCheckBox("elementosForm:comidaFavorita:2");
+	}
+	
+	public void setComidaVegetariano() {
+		dsl.clicarCheckBox("elementosForm:comidaFavorita:3");
+	}
+	
+	public void setEscolaridade(String escolaridade) {
+		dsl.selecionarCombo("elementosForm:escolaridade", escolaridade);
+	}
+	
+	public void setEsporte(String... esportes) {
+		for(String esporte: esportes)
+			dsl.selecionarCombo("elementosForm:esportes", esporte);
+	}
+	
+	public void setSugestoes(String sugestoes) {
+		dsl.escrever("elementosForm:sugestoes", sugestoes);
+	}
+	
+	public void cadastrar() {
+		dsl.clicarBotao("elementosForm:cadastrar");
+	}
+	
+	public String obterResultadoCadastro() {
+		return dsl.pegarTexto("resultado");
+	}
+	
+	public String obterNomeCadastrado() {
+		return dsl.pegarTexto("descNome");
+	}
+	
+	public String obterSobrenomeCadastrado() {
+		return dsl.pegarTexto("descSobrenome");
+	}
+	
+	public String obterSexoCadastrado() {
+		return dsl.pegarTexto("descSexo");
+	}
+	
+	public String obterComidaCadastrado() {
+		return dsl.pegarTexto("descComida");
+	}
+	
+	public String obterEscolaridadeCadastrado() {
+		return dsl.pegarTexto("descEscolaridade");
+	}
+	
+	public String obterEsporteCadastrado() {
+		return dsl.pegarTexto("descEsportes");
+	}
+	
+	public String obterSugestoesCadastradas() {
+		return dsl.pegarTexto("descSugestoes");
+	}
+	
+}
